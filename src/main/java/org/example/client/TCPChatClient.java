d7fa5f053e6155a1d5b85b7ef783ec8ff5193e3c
Willian Mikhael da Cunha <willian_mikhael@outlook.com>
2023-06-12 20:38:04 -0300

    CreateChat

---
+++ src/main/java/org/example/client/TCPChatClient.java
@@ -0,0 +1,65 @@
+package org.example.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TCPChatClient {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedReader consoleReader;
+
+    public void start(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Connected to the server. Start typing your messages.");
+
+        // Start a separate thread to listen for server messages
+        Thread messageListener = new Thread(this::listenForMessages);
+        messageListener.start();
+
+        // Send messages to the server
+        String userInput;
+        while ((userInput = consoleReader.readLine()) != null) {
+            out.println(userInput);
+        }
+    }
+
+    private void listenForMessages() {
+        try {
+            String serverResponse;
+            while ((serverResponse = in.readLine()) != null) {
+                System.out.println("Server: " + serverResponse);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            System.out.println("Error while closing the connection.");
+        }
+    }
+
+    public static void main(String[] args) {
+        TCPChatClient client = new TCPChatClient();
+        try {
+            client.start("127.0.0.1", 12345);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            client.stop();
+        }
+    }
+}
---
+++ src/main/java/org/example/server/TCPChatServer.java
@@ -0,0 +1,75 @@
+package org.example.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPChatServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedReader consoleReader;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server started. Waiting for a client to connect...");
+
+        clientSocket = serverSocket.accept();
+        System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Client connected. Start typing your messages.");
+
+        // Start a separate thread to listen for client messages
+        Thread messageListener = new Thread(this::listenForMessages);
+        messageListener.start();
+
+        // Send messages to the client
+        String userInput;
+        while ((userInput = consoleReader.readLine()) != null) {
+            out.println(userInput);
+        }
+    }
+
+    private void listenForMessages() {
+        try {
+            String clientMessage;
+            while ((clientMessage = in.readLine()) != null) {
+                System.out.println("Client: " + clientMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            System.out.println("Error while closing the connection.");
+        }
+    }
+
+    public static void main(String[] args) {
+        TCPChatServer server = new TCPChatServer();
+        try {
+            server.start(12345);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            server.stop();
+        }
+    }
+}
+
+
